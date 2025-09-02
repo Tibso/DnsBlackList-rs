8fcdd148d503e74a847c53a3caca5ec1706a204a
Tibso <thibaut.diels@conostix.com>
2025-09-02 14:56:21 +0200

    Fix Dockerfile + startup.sh

--- DnsBlackList-rs/startup.sh
+++ DnsBlackList-rs/startup.sh
@@ -9,9 +9,11 @@ MARKER_FILE=/var/lib/dnsblrsd/.blacklist_initialized
 
 if [ ! -f "$MARKER_FILE" ]; then
   echo "Running blacklist initialization..."
-  /usr/local/bin/redis-ctl /etc/dnsblrsd/dnsblrsd.conf feed-from-downloads /var/lib/dnsblrsd/blacklist-sources.json 3M
+  /usr/local/bin/redis-ctl /etc/dnsblrsd/dnsblrsd.conf feed-from-downloads /var/lib/dnsblrsd/blacklist_sources.json 3M
   touch "$MARKER_FILE"
 fi
 
 echo "Starting dnsblrsd"
-exec /usr/local/bin/dnsblrsd &
+/usr/local/bin/dnsblrsd &
+
+tail -f /dev/null
